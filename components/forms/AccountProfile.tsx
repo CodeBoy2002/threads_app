6ccf327a4a9ef34a50dc6deb5984021bbc802c90
Sumit <marvelsumit2002@gmail.com>
2023-10-18 18:35:52 +0530

    complete all the features

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -139,7 +139,7 @@ const AccountProfile = ({ user, btnTitle }: Props)=> {
                     onChange={(e)=> handleImage(e, field.onChange)}
                 />
               </FormControl>
-              <FormMessage/>
+              
             </FormItem>
           )}
         />
--- components/forms/PostThread.tsx
+++ components/forms/PostThread.tsx
@@ -13,6 +13,7 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import { Button } from "@/components/ui/button"
 import { Textarea } from "@/components/ui/textarea"
 import { usePathname, useRouter } from 'next/navigation'
+import { useOrganization } from '@clerk/nextjs'
 
 import { ThreadValidation } from "@/lib/validations/thread"
 import { updateUser } from "@/lib/actions/user.actions"
@@ -34,6 +35,7 @@ interface Props {
 function PostThread({ userId }: { userId: string }) {
     const router = useRouter()
     const pathname = usePathname()
+    const { organization } = useOrganization()
 
     const form = useForm({
         resolver: zodResolver(ThreadValidation),
@@ -47,7 +49,7 @@ function PostThread({ userId }: { userId: string }) {
         await createThread({
             text: values.thread, 
             author: userId,
-            communityId: null,
+            communityId: organization ? organization.id : null,
             path: pathname
         })
 
